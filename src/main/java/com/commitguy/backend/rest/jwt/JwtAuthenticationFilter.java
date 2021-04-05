18eb0aa48503759f75537d9121ec83363f868d69
Javier Cancela <javiciier@gmail.com>
2021-04-05 21:08:50 +0200

    [SECURITY] Security implementation for Users endpoints.
    Created JWT Authentication.
    Implemented basic Spring Security.

---
+++ src/main/java/com/commitguy/backend/rest/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,62 @@
+package com.commitguy.backend.rest.jwt;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/* www.freecodecamp.org/news/how-to-setup-jwt-authorization-and-authentication-in-spring */
+public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+    private JwtGenerator jwtGenerator;
+
+    public JwtAuthenticationFilter(AuthenticationManager authManager, JwtGenerator jwtGenerator) {
+        super(authManager);
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @Override
+    /* Obtiene el JWT de la cabecera HTTP AUTHORIZATION y se lo comunica a Spring para que configure el contexto del usuario */
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        // Comprueba si se ha recibido el JWT en la cabecera de la petición
+        String authHeader = req.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            chain.doFilter(req, res);
+            return;
+        }
+
+        // Al obtener el JWT, extrae sus atributos y se los comunica a Spring
+        String token = authHeader.replace("Bearer ", "");                   // Elimina el "Bearer " de la cabecera
+        UsernamePasswordAuthenticationToken authenticationToken = getAuthentication(req);    // Obtiene los datos de acceso
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);           // COnfigura Spring con los datos recibidos
+    }
+
+    /* Extrae los datos de acceso del JWT */
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest req) {
+        String token = req.getHeader(HttpHeaders.AUTHORIZATION);
+
+        // Extraer los datos del token
+        if (token != null) {
+            JwtInfo data = jwtGenerator.getInfoFromToken(token);
+
+            // Si se obtienen los datos, se registran
+            if (data != null) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                return new UsernamePasswordAuthenticationToken(data,null, authorities);
+            } else
+                return null;
+        }
+
+        return null;
+    }
+}
