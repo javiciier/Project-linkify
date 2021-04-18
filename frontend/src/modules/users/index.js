c75af448ad5fa46b647b72cc3f707f734dbc8d6f
Javier Cancela <javiciier@gmail.com>
2021-04-18 21:25:15 +0200

    [FRONTEND USER REDUX_REDUCER] Implemented user state reducer.
    Pending starting components development

---
+++ frontend/src/modules/users/index.js
@@ -0,0 +1,6 @@
+import * as actions from './redux/actions';
+import * as actionTypes from './redux/actionTypes';
+import reducer from './redux/reducer';
+
+
+export {actions, actionTypes, reducer};
---
+++ frontend/src/modules/users/redux/reducer.js
@@ -0,0 +1,36 @@
+import {combineReducers} from 'redux';
+import * as actionTypes from './actionTypes';
+
+const defaultState = {
+    user: null
+}
+
+
+/**
+ * Máquina de estados para el estado del usuario
+ * @param {Object} state - Estado actual
+ * @param {String} action - Acción a ejecutar
+ */
+const user = (state = defaultState.user, action) => {
+    switch (action.type) {
+        case actionTypes.LOGIN_COMPLETED:
+            return action.loggedInUser.user;
+        
+        case actionTypes.LOGOUT_COMPLETED:
+            return defaultState.user;
+        
+        case actionTypes.SIGN_UP_COMPLETED:
+            return action.signedUpUser.user;
+        
+        case actionTypes.UPDATE_PROFILE_COMPLETED:
+            return action.updatedUser;
+
+        default:
+            return defaultState;
+    }
+}
+
+
+export const reducer = combineReducers({
+    user
+});
